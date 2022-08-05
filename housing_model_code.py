24b194bce2412a4032ad4baf3ad46eba394479d1
Toffick Mohammed <toffickm252@gmail.com>
2022-08-05 09:28:18 +0000

    modeL-building
    Cleaned out some categorical columns
    Checked description of data
    started with feature engineering

--- housing_model_code.py
+++ housing_model_code.py
@@ -8,13 +8,22 @@ Created on Thu Aug  4 22:04:23 2022
 import numpy as np
 import pandas as pd 
 
-housing=pd.read_csv("C:\\Users\\TOFFICK\\Documents\\Housing_dataset.csv")
+housing=pd.read_csv("C:\\Users\\TOFFICK\\Documents\\GitHub\\Housing_data_analysis\\Housing_dataset_cleaned.csv")
 
-print(housing.head())
+#print(housing.head())
 
 #print(housing["size"].head())
-print(housing.info())
+#print(housing.info())
 
 housing["price"].astype("float")
 
 ### Feature Enineering
+## Dropping categorical columns
+#housing_dropped=pd.drop(['availability','location','size_comp'],axis=1)
+housing.drop(['availability', 'location','size_comp'], axis = 1, inplace = True)
+
+#print(housing.head())
+
+## Feature engineering begins 
+print(housing.describe())
+
